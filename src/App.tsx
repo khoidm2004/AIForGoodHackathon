c2c36de84fac833c85525e409bf0891c873171d0
Nhi Nguyen <lamnhi.ngngoc@gmail.com>
2026-05-22 14:17:38 +0300

    Add toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import MainLayout from './layouts/main_layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <MainLayout>
-      <div className="mx-auto max-w-4xl px-4 py-8 text-center">
-        <h1 className="mb-4 text-3xl font-bold text-gray-900 dark:text-white">
-          This is the main body content
-        </h1>
-        <p className="text-gray-600 dark:text-gray-300">
-          Header và footer được quản lý bởi MainLayout.<br />
-        </p>
-      </div>
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<p>Home page content</p>} />
+          <Route path="/about" element={<p>About page content</p>} />
+          <Route path="/contact" element={<p>Contact page content</p>} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   )
 }
 
